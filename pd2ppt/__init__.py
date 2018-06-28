950cc36f7768ac1c657c4e4478d3fad93425e8aa
Faisal Dosani <faisal.dosani@gmail.com>
2018-06-27 22:28:12 -0400

    moving python code base into module and tests folder

---
+++ pd2ppt/__init__.py
@@ -0,0 +1,2 @@
+from .pd2ppt import df_to_powerpoint
+from .pd2ppt import df_to_table
---
+++ tests/test_pd2ppt.py
@@ -0,0 +1,21 @@
+import os
+import pandas as pd
+
+from pd2ppt import df_to_powerpoint
+
+
+
+def test_integration_df_to_powerpoint():
+    df = pd.DataFrame(
+        {'District':['Hampshire', 'Dorset', 'Wiltshire', 'Worcestershire'],
+         'Population':[25000, 500000, 735298, 12653],
+         'Ratio':[1.56, 7.34, 3.67, 8.23]})
+
+    df_to_powerpoint(
+        r"test1.pptx", df,
+        col_formatters=['', ',', '.2'], rounding=['', 3, ''])
+
+    df_to_powerpoint(
+        r"test2.pptx", df,
+        left=1, top=1, width=10, height=15,
+        col_formatters=['', ',', '.2'], rounding=['', 3, ''])
